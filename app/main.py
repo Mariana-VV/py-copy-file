e921648ac390d244bcfdbb807659160f903b66bf
Mariana Trinko <marjanavv@gmail.com>
2026-02-20 17:28:15 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -6,13 +6,15 @@ def copy_file(command: str) -> None:
     if len(parts) == 3 and parts[0] == "cp":
         file_src = parts[1]
         file_dst = parts[2]
-        if file_src == file_dst: return
+        if file_src == file_dst:
+            return
         try:
             if not os.path.isfile(file_src):
                 return
-            with open(file_src, "r") as file_in, open(file_dst, "w") as file_to:
+            with (open(file_src, "r") as file_in,
+                  open(file_dst, "w") as file_to):
                 content = file_in.read()
                 file_to.write(content)
-        except  OSError:
+        except OSError:
             return None
     return None
